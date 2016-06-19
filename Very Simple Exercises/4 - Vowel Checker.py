e2c905978f878669b9951f07013d7e0889afdd3d
Aaron Kazah <aaronkazah@gmail.com>
2016-06-19 18:07:32 +0100

    Improved code for 'Vowel Checker'

--- Very Simple Exercises/4 - Vowel Checker.py
+++ Very Simple Exercises/4 - Vowel Checker.py
@@ -5,7 +5,6 @@ Write a function that takes a character (i.e. a string of length 1) and returns 
 character = input('Please enter a character')
 
 def is_vowel(character):
-    character.lower()
     if character in 'AaEeIiOoUU':
         print("It's a vowel!")
     else:
