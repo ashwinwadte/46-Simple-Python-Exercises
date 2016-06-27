d007378b79bfcf355fa0bef3cd4a755bb9e14860
Aaron Kazah <aaronkazah@gmail.com>
2016-06-27 22:19:00 +0100

    Compelted exercise 21 - Char Frequency

---
+++ Very Simple Exercises/21 - Char Frequency.py
@@ -0,0 +1,19 @@
+'''
+Write a function char_freq() that takes a string and builds a frequency listing of the characters contained in it.
+Represent the frequency listing as a Python dictionary. Try it with something like char_freq("abbabcbdbabdbdbabababcbcbab").
+'''
+
+a_string = 'abbabcbdbabdbdbabababcbcbab'
+
+def char_freq(a_string):
+    frequency_dict = {
+
+    }
+    for i in a_string:
+        if i in frequency_dict:
+            frequency_dict[i] += 1
+        else:
+            frequency_dict[str(i)] = 1
+    print (frequency_dict)
+
+char_freq(a_string)
