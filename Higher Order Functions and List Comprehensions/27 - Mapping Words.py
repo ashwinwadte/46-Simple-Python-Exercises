345dba7f7feb8cf86ff625a26817121b7c0dfbe3
Aaron <aaronkazah@gmail.com>
2016-09-07 21:46:00 +0100

    Completed Exercise 27 - Mapping Words

---
+++ Higher Order Functions and List Comprehensions/27 - Mapping Words.py
@@ -0,0 +1,23 @@
+'''
+Write a program that maps a list of words into a list of integers representing the lengths of the correponding words.
+Write it in three different ways: 1) using a for-loop, 2) using the higher order function map(), and 3) using list comprehensions.
+'''
+
+example = ['Chicken','Dragon','Palindromes','JackieChan']
+
+def length_of_words(list):
+    word_lengths = []
+    for word in list:
+        word_lengths.append(len(word))
+    return word_lengths
+
+def length_of_words1(list):
+    return map(len, list)
+
+def length_of_words2(list):
+    return ([len(word) for word in list])
+
+
+print (length_of_words(example))
+print (length_of_words1(example))
+print (length_of_words2(example))
