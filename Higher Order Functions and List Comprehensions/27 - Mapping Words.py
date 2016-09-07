979d323df096f974a336ca7747afa00f49fb97e9
Aaron <aaronkazah@gmail.com>
2016-09-07 21:47:11 +0100

    added comments

--- Higher Order Functions and List Comprehensions/27 - Mapping Words.py
+++ Higher Order Functions and List Comprehensions/27 - Mapping Words.py
@@ -5,15 +5,18 @@ Write it in three different ways: 1) using a for-loop, 2) using the higher order
 
 example = ['Chicken','Dragon','Palindromes','JackieChan']
 
+# Mapping words using a for-loop
 def length_of_words(list):
     word_lengths = []
     for word in list:
         word_lengths.append(len(word))
     return word_lengths
 
+# Mapping words using the higher order function map()
 def length_of_words1(list):
     return map(len, list)
 
+# Mapping words using a list comprehensions
 def length_of_words2(list):
     return ([len(word) for word in list])
 
