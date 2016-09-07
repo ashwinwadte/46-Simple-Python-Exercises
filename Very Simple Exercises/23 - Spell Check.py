ef66dd6cc088679ba9689cb2707c1d14298d9dfc
Aaron <aaronkazah@gmail.com>
2016-09-07 16:39:32 +0100

    completed exercise 23

---
+++ Very Simple Exercises/23 - Spell Check.py
@@ -0,0 +1,18 @@
+'''
+Define a simple "spelling correction" function correct()
+that takes a string and sees to it that
+1) two or more occurrences of the space character is compressed into one, and
+2) inserts an extra space after a period if the period is directly followed by a letter.
+E.g. correct("This is very funny and cool.Indeed!") should return "This is very funny and cool.
+Indeed!" Tip: Use regular expressions!
+'''
+
+import re
+
+example = "This is very      funny and    cool.Indeed!"
+def correct(a_string):
+    check_spaces = re.sub(r'\s{2,}',' ',a_string)
+    make_spaces = re.sub(r'(\.)(\w)', r'\1 \2',check_spaces)
+    return make_spaces
+
+print correct(example)
