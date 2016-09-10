eae5c23687446b1c65c322a1385c7740e1e54374
Aaron <aaronkazah@gmail.com>
2016-09-10 01:00:49 +0100

    started exercise 31

---
+++ Very Simple Exercises/31 - Student Becomes Master.py
@@ -0,0 +1,16 @@
+'''
+Implement the higher order functions map(), filter() and reduce().
+(They are built-in but writing them yourself may be a good exercise.)
+'''
+
+# The solutions below are were based on this definition from http://www.python-course.eu/lambda.php
+example = ['Apple','Jack','Google']
+
+
+# implementation of the map() function
+def my_map(function, sequence):
+    result = []
+    for i in sequence:
+        result.append(function(i))
+    return result
+
