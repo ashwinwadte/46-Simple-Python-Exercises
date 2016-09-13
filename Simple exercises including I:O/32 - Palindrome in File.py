eb69e5b84ec8e291ea5c683f7a23ea79763b626d
Aaron <aaronkazah@gmail.com>
2016-09-13 15:33:31 +0100

    Completed exercise 32

---
+++ Simple exercises including I:O/32 - Palindrome in File.py
@@ -0,0 +1,22 @@
+'''
+Write a version of a palindrome recogniser that accepts a file name from the user,
+reads each line, and prints the line to the screen if it is a palindrome.
+'''
+import re
+
+def palindrome_checker(text):
+    text = re.sub(r'[^a-zA-Z]', "",text).lower()
+    if text == text[::-1]:
+        return True
+    else:
+        return False
+
+def file_opener(filename):
+    f = open(filename, 'r')
+    for line in f:
+        if palindrome_checker(line):
+            print(line)
+        else:
+            pass
+
+print file_opener('palindromes.txt')
