73d05904f5684eadfe9c03ae285fe8113d70ccf8
Aaron <aaronkazah@gmail.com>
2016-09-14 20:28:00 +0100

    Improved exercise 34 to print characters  frequency in a sorted order

--- Simple exercises including I:O/34 - Terminal Character Frequency.py
+++ Simple exercises including I:O/34 - Terminal Character Frequency.py
@@ -3,7 +3,7 @@ Write a procedure char_freq_table() that, when run in a terminal,
 accepts a file name from the user, builds a frequency listing of the characters contained in the file,
 and prints a sorted and nicely formatted character frequency table to the screen.
 '''
-
+import re
 file_name = raw_input('please enter your file name')
 
 def char_freq_table(file_name):
@@ -11,13 +11,16 @@ def char_freq_table(file_name):
     }
     f = open(file_name,'r')
     for line in f:
+        #ignores all white space
+        line = re.sub(r'[\s]', "", line).lower()
         for char in line:
             if char in frequency_dict:
                 frequency_dict[char] += 1
             else:
                 frequency_dict[str(char)] = 1
     print 'Here are the results'
-    for char in sorted(frequency_dict.items()):
+    #sorts character in a descending order - greatest to smallest
+    for char in sorted(frequency_dict.items(),key=lambda x: x[1],reverse=True):
         print char[0], frequency_dict[char[0]]
 
 
