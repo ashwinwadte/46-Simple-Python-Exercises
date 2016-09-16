4d94568670b61a834dbf0b60bedae563796dee06
Aaron <aaronkazah@gmail.com>
2016-09-17 00:32:08 +0100

    Completed exercise 36- Hapaxes

--- Simple exercises including I:O/36 - Hapax Legomenon.py
+++ Simple exercises including I:O/36 - Hapax Legomenon.py
@@ -7,12 +7,14 @@ Make sure your program ignores capitalization.
 import re
 def hapax_checker(file_name):
     f = open(file_name, 'r').read()
+    #substitutue all non letters with spaces
     f = re.sub(r'[^A-Za-z]', ' ', f)
     words = f.lower().split(' ')
     print(words)
 
     for line in f:
         words.append(line.lower().split())
+    #filter swords which occurs once
     hapaxes = list(filter(lambda x: words.count(x) == 1, words))
     return hapaxes
 
